f34a4806ead544d3903ffed48f0fca97f92c6a21
warriorsaga <tozodas854@gmail.com>
2022-01-13 00:22:56 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>iEducate - Transforming Online Education</title>
+</head>
+<body>
+    <nav class="navbar background h-nav-resp">
+        <ul class="nav-list v-class-resp">
+            <div class="logo"><img src="img/logo.jpg" alt="logo"></div>
+            <li> <a href="#">Home</a></li>
+            <li> <a href="#About">About</a></li>
+            <li> <a href="#Services">Services</a></li>
+            <li> <a href="#Contact">Contact</a></li>
+        </ul>
+        <div class="rightnav v-class-resp">
+            <input type="text" name="serach" id="search">
+            <button class="btn btn-sm">Search</button>
+        </div>
+        <div class="burger">
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+        </div>
+    </nav>
+
+    <section class="background firstSection">
+        <div class="box-main">
+            <div class="firstHalf">
+                <p class="text-big">The Future of Education is here</p>
+                <p class="text-small">In this world of 7 billion people we need to educate all of them.This is the future of an educated world and we are proud to say that The Future od Education is here</p>
+                <div class="buttons">
+                    <button class="btn">Subscribe</button>
+                    <button class="btn">Watch Video</button>
+                </div>
+            </div>
+            <div class="secondHalf">
+                <img src="img/logo.jpg" alt="Laptop Image">
+            </div>
+        </div>
+    </section>
+
+    <section class="section">
+        <div class="paras">
+        <p class="sectionTag text-big">The end Of Your Search Is Here</p>
+        <p class="sectionSubTag text-small">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ducimus numquam, ut eos tempora explicabo fugit error minus consequatur reiciendis. Quaerat omnis nulla et assumenda, magnam sint obcaecati excepturi impedit distinctio? Veritatis molestias voluptatibus similique soluta, autem obcaecati explicabo. Possimus, explicabo. Nesciunt repellendus magni odio consectetur minus omnis at harum iste, veritatis, odit aliquam deleniti ipsa explicabo molestias, eaque magnam impedit ullam aut dignissimos nulla eos officia culpa. Expedita facilis animi sint laboriosam molestiae, dolor, perferendis ab ea hic a sapiente quo pariatur ratione. Ratione dolore dicta minima facilis neque repellendus molestiae autem deserunt. Dignissimos esse, voluptatibus unde vitae ex voluptates illum quibusdam cumque porro, placeat adipisci! Reprehenderit modi aperiam neque asperiores velit autem voluptas. Perferendis, exercitationem, autem, atque laborum sequi mollitia dolore perspiciatis corrupti quibusdam maxime dolorum quaerat ratione inventore officiis iste voluptatibus sit labore. Quia expedita perferendis adipisci ducimus veniam! Consequatur iusto tempore, molestias eligendi harum perferendis ullam necessitatibus voluptas aperiam fugiat minus voluptatibus corrupti fuga a illum aliquid voluptatem officiis </p>
+    </div>
+        <div class="thumbnail">
+            <img src="https://source.unsplash.com/900x900/?coding,apple" alt="Laptop image" class="imgFluid">
+        </div>
+    </section>
+
+    <section class="section section-left" id="Services">
+        <div class="paras">
+        <p class="sectionTag text-big">Transforming Education in India</p>
+        <p class="sectionSubTag text-small">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ducimus numquam, ut eos tempora explicabo fugit error minus consequatur reiciendis. Quaerat omnis nulla et assumenda, magnam sint obcaecati excepturi impedit distinctio? Veritatis molestias voluptatibus similique soluta, autem obcaecati explicabo. Possimus, explicabo. Nesciunt repellendus magni odio consectetur minus omnis at harum iste, veritatis, odit aliquam deleniti ipsa explicabo molestias, eaque magnam impedit ullam aut dignissimos nulla eos officia culpa. Expedita facilis animi sint laboriosam molestiae, dolor, perferendis ab ea hic a sapiente quo pariatur ratione. Ratione dolore dicta minima facilis neque repellendus molestiae autem deserunt. Dignissimos esse, voluptatibus unde vitae ex voluptates illum quibusdam cumque porro, placeat adipisci! Reprehenderit modi aperiam neque asperiores velit autem voluptas. Perferendis, exercitationem, autem, atque laborum sequi mollitia dolore perspiciatis corrupti quibusdam maxime dolorum quaerat ratione inventore officiis iste voluptatibus sit labore. Quia expedita perferendis adipisci ducimus veniam! Consequatur iusto tempore, molestias eligendi harum perferendis ullam necessitatibus voluptas aperiam fugiat minus voluptatibus corrupti fuga a illum aliquid voluptatem officiis </p>
+    </div>
+        <div class="thumbnail">
+            <img src="https://source.unsplash.com/900x900/?coding,apple,html" alt="Laptop image" class="imgFluid">
+        </div>
+    </section>
+
+    <section class="section" id="About">
+        <div class="paras">
+        <p class="sectionTag text-big">Lets Grow Together</p>
+        <p class="sectionSubTag text-small">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ducimus numquam, ut eos tempora explicabo fugit error minus consequatur reiciendis. Quaerat omnis nulla et assumenda, magnam sint obcaecati excepturi impedit distinctio? Veritatis molestias voluptatibus similique soluta, autem obcaecati explicabo. Possimus, explicabo. Nesciunt repellendus magni odio consectetur minus omnis at harum iste, veritatis, odit aliquam deleniti ipsa explicabo molestias, eaque magnam impedit ullam aut dignissimos nulla eos officia culpa. Expedita facilis animi sint laboriosam molestiae, dolor, perferendis ab ea hic a sapiente quo pariatur ratione. Ratione dolore dicta minima facilis neque repellendus molestiae autem deserunt. Dignissimos esse, voluptatibus unde vitae ex voluptates illum quibusdam cumque porro, placeat adipisci! Reprehenderit modi aperiam neque asperiores velit autem voluptas. Perferendis, exercitationem, autem, atque laborum sequi mollitia dolore perspiciatis corrupti quibusdam maxime dolorum quaerat ratione inventore officiis iste voluptatibus sit labore. Quia expedita perferendis adipisci ducimus veniam! Consequatur iusto tempore, molestias eligendi harum perferendis ullam necessitatibus voluptas aperiam fugiat minus voluptatibus corrupti fuga a illum aliquid voluptatem officiis </p>
+    </div>
+        <div class="thumbnail">
+            <img src="https://source.unsplash.com/900x900/?javascript,apple,nature,happy" alt="Laptop image" class="imgFluid">
+        </div>
+    </section>
+    <hr>
+    <section class="contact" id="Contact">
+        <h2 class="text-center">Contact Us</h2>
+        <?php
+            //  echo "We are successfully connected";
+
+             if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+
+                $name = $_POST['name'];
+                $phone = $_POST['phone'];
+                $email = $_POST['email'];
+                $desc = $_POST['text'];
+
+
+                $server = "localhost";
+                $username = "root";
+                $password = "";
+                $database = "ieducate";
+
+                $conn = mysqli_connect($server, $username, $password, $database);
+                
+                if(!$conn){
+                    echo "The database is not successfull connected because of". mysqli_connect_error();
+                }
+                else{
+                    // echo "The data is successfully connected";
+
+                $sql = "INSERT INTO `students` ( `name`, `phone`, `email`, `descrp`, `dt`) VALUES ( '$name', '$phone', '$email', '$desc', current_timestamp())";
+
+                $result = mysqli_query($conn,$sql);
+
+                if($result){
+                    echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <div class="mx-auto" style="width: 390px;">
+                    <strong>Success! Your data has been successfully submitted</strong> 
+                    </div>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                  </div>';
+                }
+                else{
+                    echo "Data not submitted successfully";
+                }
+
+                }
+
+             }
+
+        ?>
+        <div class="form">
+            <form action="/Experiment Website/index.php", method="post" class="myForm" id="Contact">
+            <input type="text" class="form-input" name="name" id="name" placeholder="Enter Your name">
+            <input type="phone" class="form-input" name="phone" id="phone" placeholder="Enter Your Phone">
+            <input type="email" class="form-input"name="email" id="email" placeholder="Enter Your emial">
+            <textarea name="text" class="form-input" id="text" cols="30" rows="10" placeholder="Ellaborate your concern"></textarea>
+            <button class="btn btn-sm btn-dark">Submit</button>
+            </form>
+        </div>
+    </section>
+
+    <footer class="background">
+        <p class="text-footer">
+            Copyright &copy; 2027 www.iEducate.com - All rights reserved
+        </p>
+    </footer>
+    <script src="js/resp.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+</body>
+</html>
